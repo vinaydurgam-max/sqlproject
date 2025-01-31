16fadd18ff91dbfe74c204be4d9f2b4586c0eb8c
DURGAM VINAY KUMAR <durgamvinay43@gmail.com>
2025-01-31 15:30:49 +0530

    Add files via upload

---
+++ ML WEEK 1.py
@@ -0,0 +1,4 @@
+PFIA=float(input("Enter the probablity it is friday and that a student is absent:"))
+PF = float(input("Probablity that is friday:"))
+PABF = PFIA/PF
+print("Probablity that a student is absent given that today is friday using conditional probablities:",PABF)
